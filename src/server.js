1554c77922baeb3f7fc2ede3b4b62581b7badbc4
Shifyannn <asifyan01@gmail.com>
2024-05-13 23:04:48 +0700

    Penambahan route POST

--- src/server.js
+++ src/server.js
@@ -9,22 +9,67 @@ const init = async () => {
     method: "POST",
     path: "/books",
     handler: (request, h) => {
-      //   const {
-      //     name,
-      //     year,
-      //     author,
-      //     summary,
-      //     publisher,
-      //     pageCount,
-      //     readPage,
-      //     reading,
-      //   } = request.payload;
-
+      const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+      } = request.payload;
+      const isBookFinished = (pageCount, readPage) => {
+        if (pageCount === readPage) {
+          return true;
+        } else {
+          return false;
+        }
+      };
       const book = {
         id: Nanoid.nanoid(),
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        finished: isBookFinished(pageCount, readPage),
+        reading,
+        insertedAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
       };
-      console.log(book.id);
-      return "Data Berhasil Ditambahkan";
+      let response;
+
+      if (!name) {
+        response = h
+          .response({
+            status: "fail",
+            message: "Gagal menambahkan buku. Mohon isi nama buku",
+          })
+          .code(400);
+      } else if (readPage > pageCount) {
+        response = h
+          .response({
+            status: "fail",
+            message:
+              "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
+          })
+          .code(400);
+      } else {
+        response = h
+          .response({
+            status: "success",
+            message: "Buku berhasil ditambahkan",
+            data: {
+              bookId: book.id,
+            },
+          })
+          .code(201);
+      }
+
+      return response;
     },
   });
   await server.start();
