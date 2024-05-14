bbdea006d21dbaae0c97d50a8fbb74e5cbfe06f1
Shifyannn <asifyan01@gmail.com>
2024-05-14 12:26:16 +0700

    penambahan rute

--- src/server.js
+++ src/server.js
@@ -102,6 +102,131 @@ const init = async () => {
         return response;
       },
     },
+    {
+      method: "GET",
+      path: "/books/{bookId}",
+      handler: (request, h) => {
+        const findResult = books.find(
+          (book) => book.id === request.params.bookId
+        );
+        if (findResult === undefined) {
+          const response = h
+            .response({
+              status: "fail",
+              message: "Buku tidak ditemukan",
+            })
+            .code(404);
+          return response;
+        } else {
+          const response = h
+            .response({
+              status: "success",
+              data: {
+                book: findResult,
+              },
+            })
+            .code(200);
+          return response;
+        }
+      },
+    },
+    {
+      method: "PUT",
+      path: "/books/{bookId}",
+      handler: (request, h) => {
+        const { bookId } = request.params;
+        const {
+          name,
+          year,
+          author,
+          summary,
+          publisher,
+          pageCount,
+          readPage,
+          reading,
+        } = request.payload;
+
+        const index = books.findIndex((book) => book.id === bookId);
+        if (index === -1) {
+          return h
+            .response({
+              status: "fail",
+              message: "Gagal memperbarui buku. Id tidak ditemukan",
+            })
+            .code(404);
+        }
+
+        if (!name) {
+          return h
+            .response({
+              status: "fail",
+              message: "Gagal memperbarui buku. Mohon isi nama buku",
+            })
+            .code(400);
+        }
+
+        if (readPage > pageCount) {
+          return h
+            .response({
+              status: "fail",
+              message:
+                "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+            })
+            .code(400);
+        }
+
+        books[index] = {
+          ...books[index],
+          name,
+          year,
+          author,
+          summary,
+          publisher,
+          pageCount,
+          readPage,
+          reading,
+          updatedAt: new Date().toISOString(),
+        };
+
+        return h
+          .response({
+            status: "success",
+            message: "Buku berhasil diperbarui",
+          })
+          .code(200);
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/books/{bookId}",
+      handler: (request, h) => {
+        const findResult = books.find(
+          (book) => book.id === request.params.bookId
+        );
+
+        if (findResult === undefined) {
+          const response = h
+            .response({
+              status: "fail",
+              message: "Buku gagal dihapus. Id tidak ditemukan",
+            })
+            .code(404);
+          return response;
+        } else {
+          const booksIndex = books.findIndex(
+            (book) => book.id === request.params.bookId
+          );
+          books.splice(booksIndex, 1);
+          const response = h
+            .response({
+              status: "Success",
+              message: "Buku berhasil dihapus",
+            })
+            .code(200);
+          return response;
+        }
+      },
+    },
   ]);
   await server.start();
   console.log(`Server is Running in Port ${server.info.port}`);
